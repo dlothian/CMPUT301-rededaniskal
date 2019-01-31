77ee56673ec778890265373e784607d9bbdfde93
Daniela T <danielat@Danielas-MacBook-Air.local>
2019-01-31 14:41:34 -0700

    First commit Rectangle class

---
+++ GroupApplication/app/src/main/java/com/example/groupapplication/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle extends Shape{
+	public void Rectangle(){
+		System.out.println("I am a rectangle!");
+	}
+	
+	public void Rectangle(String color){
+		this.color = color;
+	}
+}
